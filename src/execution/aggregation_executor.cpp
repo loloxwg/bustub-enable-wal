4dfbc218e986411f63b978eacd54b5a2c3f282e4
lssssj <1169064849@qq.com>
2024-10-21 11:00:31 +0800

    finish Aggregation

--- src/execution/aggregation_executor.cpp
+++ src/execution/aggregation_executor.cpp
@@ -10,19 +10,75 @@
 //
 //===----------------------------------------------------------------------===//
 #include <memory>
+#include <utility>
 #include <vector>
 
+#include "common/rid.h"
 #include "execution/executors/aggregation_executor.h"
+#include "execution/plans/aggregation_plan.h"
+#include "storage/table/tuple.h"
+#include "type/integer_type.h"
+#include "type/value_factory.h"
 
 namespace bustub {
 
 AggregationExecutor::AggregationExecutor(ExecutorContext *exec_ctx, const AggregationPlanNode *plan,
                                          std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx) {
+    plan_ = plan;
+    child_executor_ = std::move(child_executor);
+    aht_ = std::make_unique<SimpleAggregationHashTable>(plan_->GetAggregates(), plan_->GetAggregateTypes());
+  }
 
-void AggregationExecutor::Init() {}
+void AggregationExecutor::Init() {
+  child_executor_->Init();
+  Tuple tuple;
+  RID rid;
+  while (child_executor_->Next(&tuple, &rid)) {
+    AggregateValue agg_value;
+    AggregateKey agg_key;
+    for (const auto &expr : plan_->GetAggregates()) {
+      auto value = expr->Evaluate(&tuple, child_executor_->GetOutputSchema());
+      agg_value.aggregates_.emplace_back(value); 
+    }
+    for (const auto &expr : plan_->GetGroupBys()) {
+      auto value = expr->Evaluate(&tuple, child_executor_->GetOutputSchema());
+      agg_key.group_bys_.emplace_back(value);
+    }
+    aht_->InsertCombine(agg_key, agg_value);
+    has_value_ = true;
+  }
+  aht_iterator_ = std::make_unique<SimpleAggregationHashTable::Iterator>(aht_->Begin());
+}
 
-auto AggregationExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+auto AggregationExecutor::Next(Tuple *tuple, RID *rid) -> bool { 
+  if (!has_value_) {
+    has_value_ = true;
+    *tuple = Tuple{aht_->GenerateInitialAggregateValue().aggregates_, &GetOutputSchema()};
+    *rid = RID{0};
+    return true;
+  }
+  if (*aht_iterator_ == aht_->End()) {
+    return false;
+  }
+  if (plan_->agg_types_.size() == 1 && plan_->agg_types_[0] ==  AggregationType::CountStarAggregate) {
+    auto val = aht_iterator_->Val();
+    *tuple = Tuple{val.aggregates_, &GetOutputSchema()};
+    *rid = RID{0};
+    ++(*aht_iterator_);
+    return true;  
+  }
+  auto key = aht_iterator_->Key();
+  auto val = aht_iterator_->Val();
+  std::vector<Value> values;
+  values.reserve(key.group_bys_.size() + val.aggregates_.size());
+  values.insert(values.end(), key.group_bys_.begin(), key.group_bys_.end());
+  values.insert(values.end(), val.aggregates_.begin(), val.aggregates_.end());
+  *tuple = Tuple{values, &GetOutputSchema()};
+  *rid = RID{0};
+  ++(*aht_iterator_);
+  return true; 
+}
 
 auto AggregationExecutor::GetChildExecutor() const -> const AbstractExecutor * { return child_executor_.get(); }
 
--- src/include/execution/executors/aggregation_executor.h
+++ src/include/execution/executors/aggregation_executor.h
@@ -74,10 +74,47 @@ class SimpleAggregationHashTable {
     for (uint32_t i = 0; i < agg_exprs_.size(); i++) {
       switch (agg_types_[i]) {
         case AggregationType::CountStarAggregate:
+          result->aggregates_[i] = result->aggregates_[i].Add(ValueFactory::GetIntegerValue(1));
+          break;
         case AggregationType::CountAggregate:
+          if (input.aggregates_[i].IsNull()) {
+            continue;
+          }
+          if (result->aggregates_[i].IsNull()) {
+            result->aggregates_[i] = input.aggregates_[i];
+          } else {
+            result->aggregates_[i] = result->aggregates_[i].Add(ValueFactory::GetIntegerValue(1));
+          }
+          break;
         case AggregationType::SumAggregate:
+          if (input.aggregates_[i].IsNull()) {
+            continue;
+          }
+          if (result->aggregates_[i].IsNull()) {
+            result->aggregates_[i] = input.aggregates_[i];
+          } else {
+            result->aggregates_[i] = result->aggregates_[i].Add(input.aggregates_[i]);
+          }
+          break;
         case AggregationType::MinAggregate:
+          if (input.aggregates_[i].IsNull()) {
+            continue;
+          }
+          if (result->aggregates_[i].IsNull()) {
+            result->aggregates_[i] = input.aggregates_[i];
+          } else {
+            result->aggregates_[i] = result->aggregates_[i].Min(input.aggregates_[i]);
+          }
+          break;
         case AggregationType::MaxAggregate:
+          if (input.aggregates_[i].IsNull()) {
+            continue;
+          }
+          if (result->aggregates_[i].IsNull()) {
+            result->aggregates_[i] = input.aggregates_[i];
+          } else {
+            result->aggregates_[i] = result->aggregates_[i].Max(input.aggregates_[i]);
+          }
           break;
       }
     }
@@ -203,9 +240,13 @@ class AggregationExecutor : public AbstractExecutor {
   std::unique_ptr<AbstractExecutor> child_executor_;
 
   /** Simple aggregation hash table */
-  // TODO(Student): Uncomment SimpleAggregationHashTable aht_;
+  // TODO(Student): Uncomment 
+  std::unique_ptr<SimpleAggregationHashTable> aht_;
 
   /** Simple aggregation hash table iterator */
-  // TODO(Student): Uncomment SimpleAggregationHashTable::Iterator aht_iterator_;
+  // TODO(Student): Uncomment 
+  std::unique_ptr<SimpleAggregationHashTable::Iterator> aht_iterator_;
+
+  bool has_value_{false};
 };
 }  // namespace bustub
