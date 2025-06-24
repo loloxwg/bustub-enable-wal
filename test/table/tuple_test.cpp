f5c853440dd885a3712b0ac1718001db2c350eeb
Xwg <loloxwg@gmail.com>
2025-06-24 20:53:26 +0800

    Enable performance testing for `TableHeap` in `TupleTest` and add timing metrics for insertion and query operations.

--- test/table/tuple_test.cpp
+++ test/table/tuple_test.cpp
@@ -15,6 +15,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <chrono>
 
 #include "buffer/buffer_pool_manager.h"
 #include "gtest/gtest.h"
@@ -24,7 +25,7 @@
 
 namespace bustub {
 // NOLINTNEXTLINE
-TEST(TupleTest, DISABLED_TableHeapTest) {
+TEST(TupleTest, TableHeapTest) {
   // test1: parse create sql statement
   std::string create_stmt = "a varchar(20), b smallint, c bigint, d bool, e varchar(16)";
   Column col1{"a", TypeId::VARCHAR, 20};
@@ -34,24 +35,51 @@ TEST(TupleTest, DISABLED_TableHeapTest) {
   Column col5{"e", TypeId::VARCHAR, 16};
   std::vector<Column> cols{col1, col2, col3, col4, col5};
   Schema schema{cols};
-  Tuple tuple = ConstructTuple(&schema);
+
 
   // create transaction
   auto *disk_manager = new DiskManager("test.db");
-  auto *buffer_pool_manager = new BufferPoolManager(50, disk_manager);
+  auto *buffer_pool_manager = new BufferPoolManager(5, disk_manager);
   auto *table = new TableHeap(buffer_pool_manager);
 
+  int nums = 1000000;
+  std::vector<Tuple> tuple_v;
   std::vector<RID> rid_v;
-  for (int i = 0; i < 5000; ++i) {
+  
+  // 开始插入计时
+  auto insert_start = std::chrono::high_resolution_clock::now();
+  
+  for (int i = 0; i < nums ; ++i) {
+    Tuple tuple = ConstructTuple(&schema);
     auto rid = table->InsertTuple(TupleMeta{0, false}, tuple);
     rid_v.push_back(*rid);
+    tuple_v.push_back(tuple);
   }
+  
+  // 结束插入计时并输出结果
+  auto insert_end = std::chrono::high_resolution_clock::now();
+  auto insert_duration = std::chrono::duration_cast<std::chrono::microseconds>(insert_end - insert_start);
+  std::cout << "Insertion of " << nums << " tuples took " << insert_duration.count() << " μs" << std::endl;
+  std::cout << "Average insertion time per tuple: " << static_cast<double>(insert_duration.count()) / nums << " μs" << std::endl;
 
+  // 开始查询计时
+  auto query_start = std::chrono::high_resolution_clock::now();
+  
   TableIterator itr = table->MakeIterator();
+  int cnt = 0;
   while (!itr.IsEnd()) {
     // std::cout << itr->ToString(schema) << std::endl;
+    //EXPECT_TRUE(IsTupleContentEqual(itr.GetTuple().second,tuple_v[cnt]));
+
     ++itr;
+    ++cnt;
   }
+  
+  // 结束查询计时并输出结果
+  auto query_end = std::chrono::high_resolution_clock::now();
+  auto query_duration = std::chrono::duration_cast<std::chrono::microseconds>(query_end - query_start);
+  std::cout << "Query of " << cnt << " tuples took " << query_duration.count() << " μs" << std::endl;
+  std::cout << "Average query time per tuple: " << static_cast<double>(query_duration.count()) / cnt << " μs" << std::endl;
 
   disk_manager->ShutDown();
   remove("test.db");  // remove db file
